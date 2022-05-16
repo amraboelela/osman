3764b6e837f729a71ac5f3d1a96f8950b6391b93
Amr Aboelela <amraboelela@gmail.com>
2022-05-15 21:11:42 -0700

    language = "English"

--- subtitles.py
+++ subtitles.py
@@ -6,12 +6,21 @@ if len(sys.argv) > 2:
     prefix = sys.argv[1]
     targetLanguage = sys.argv[2]
 else:
-    print("please provide the prefix and target language")
+    print("Please provide the prefix and target language")
     exit(-1)
 
-print("## subtitles, prefix: " + prefix + ", targetLanguage: " + targetLanguage)
+#print("## subtitles, prefix: " + prefix + ", targetLanguage: " + targetLanguage)
 
-subtitlesFile = "resources/" + prefix + "-" + targetLanguage + ".srt"
-sbtOutputFile = "build/" + prefix + "-" + targetLanguage + ".mp4"
-if not path.exists(sbtOutputFile):
-    os.system("handbrakecli -i build/" + prefix + ".mp4 -o " + sbtOutputFile + " --srt-file " + subtitlesFile + " --srt-codeset UTF-8 --srt-burn")
+prefixParts = prefix.split("-")
+name = prefixParts[0]
+if name == "osman":
+    subtitlesFile = "resources/" + prefix + "-" + targetLanguage + ".srt"
+    language = "English"
+    if targetLanguage == "ar":
+        language = "Arabic"
+    targetName = "Kurulus Osman Season 3 Episode 90 - 92 With " + language + " Subtitles"
+    outputFile = "build/" + targetName + "/" + prefix + ".mp4"
+    if not path.exists(outputFile):
+        print("## subtitles, generating: " + outputFile)
+        escapedOutputFile = outputFile.replace(" ","\ ")
+        os.system("handbrakecli -i build/" + prefix + ".mp4 -o " + escapedOutputFile + " --srt-file " + subtitlesFile + " --srt-codeset UTF-8 --srt-burn")
